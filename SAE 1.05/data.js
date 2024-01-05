2928a9fe9c1320d4c78388e38e3238cd7535495a
Andie4 <155273473+Andie4@users.noreply.github.com>
2024-01-05 03:24:27 +0100

    Delete SAE 1.05 directory
    ancienne version

--- SAE 1.05/data.js
+++
@@ -1,8 +0,0 @@
-var analogies= ["un animal", "une plante", "un art", " une expression", " un film d'animation", "un paysage", "une application" ];  
-
-
-var valeurs= ["un aigle", "un bonzaï", "de la musique", "le rire", "How to train your dragon", "les montagnes au printemps", "pinterest"]
-
-
-/*boutons clicable en haut de page */
-var encre=["animal","plante","art", "expression", "film", "paysage", "application"]
